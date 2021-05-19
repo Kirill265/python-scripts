bfc3546ce399137dbf014ce4061d41587da8987c
Kirill265 <55912375+Kirill265@users.noreply.github.com>
2021-05-19 14:55:18 +0300

    keepass. deleted

--- Daily_0700.bat
+++ Daily_0700.bat
@@ -1,5 +1,3 @@
 cd C:\Users\Kirill_Cherkasov
-python.exe "C:/Users/Kirill_Cherkasov/Documents/Python_scripts/USD_14.py"
-python.exe "C:/Users/Kirill_Cherkasov/Documents/Python_scripts/Volume_USD_daily.py"
 python.exe "C:/Users/Kirill_Cherkasov/Documents/Python_scripts/Finexpert_daily_full.py"
 python.exe "C:/Users/Kirill_Cherkasov/Documents/Python_scripts/Lemon_daily_full.py"
--- Monday_0705.bat
+++ Monday_0705.bat
@@ -1,7 +1,2 @@
 cd C:\Users\Kirill_Cherkasov
-python.exe "C:/Users/Kirill_Cherkasov/Documents/Python_scripts/New_client_weekly.py"
-python.exe "C:/Users/Kirill_Cherkasov/Documents/Python_scripts/Convertation_weekly.py"
-python.exe "C:/Users/Kirill_Cherkasov/Documents/Python_scripts/Checked_cust_weekly.py"
-python.exe "C:/Users/Kirill_Cherkasov/Documents/Python_scripts/Communications.py"
-python.exe "C:/Users/Kirill_Cherkasov/Documents/Python_scripts/Volume_USD_weekly.py"
 python.exe "C:/Users/Kirill_Cherkasov/Documents/Python_scripts/24-26_monday_full.py"
